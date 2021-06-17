9ec47bbbc878ccf5f34ebc8231aa9c4f86dfc19c
Nigussie1234 <nigussieadem@gmail.com>
2021-06-17 13:06:33 -0700

    with proper grammer, but the outcomeis a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: prints "with proper grammet, but the outcome is a piece of art"
+ *
+ *Return: zero
+ */
+int main(void)
+{
+printf("\"with proper grammer, but the outcome is a piece of art");
+return (0);
+}
