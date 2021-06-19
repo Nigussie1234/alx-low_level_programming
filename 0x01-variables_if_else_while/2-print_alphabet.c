af7cb3d5c3255d17e90edf9d308dbfa5df09083f
Nigussie1234 <nigussieadem@gmail.com>
2021-06-19 04:34:56 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - display lower case alphabets
+*
+*Description: only getchar is used twice
+*Return: 0 (success)
+*/
+int main(void)
+{
+int ch;
+for(ch='a'; ch<='z', ch++)
+putchar (ch);
+putchar ('\n');
+return (0);
+}
