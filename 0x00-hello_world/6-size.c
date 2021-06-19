113d18b006a5b546edda8d6904b897e7a130bf6f
Nigussie1234 <nigussieadem@gmail.com>
2021-06-19 08:14:37 -0700

    Low level programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - prints different sizes of data
+*
+*Description: int, float, double
+*
+*Return: 0 (success)
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu bytes\n", sizeof(charType));
+return (0);
+}
