ca9f5df253ea4c92c5a0c0569de3ac38cec8a939
Nigussie1234 <nigussieadem@gmail.com>
2021-06-20 08:08:00 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - uses to give lowercase letters except q and e
+*
+*Description: the program uses only putchar
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+int i = 97;
+while ( i < 123)
+{
+if ( i != 101 && i != 113)
+{
+putchar (i);
+}
+i++;
+}
+putchar (10);
+return (0);
+}
