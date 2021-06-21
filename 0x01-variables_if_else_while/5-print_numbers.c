1bd7ebeb064d535c0584e2191abf8f6bb8bafef0
Nigussie1234 <nigussieadem@gmail.com>
2021-06-20 21:51:22 -0700

    Variables if else  while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+/**
+*Main - displays all single digit numbers
+*
+*Description: displays only numbers from 0 to 9
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+int i;
+i = 48;
+while ( i < 58)
+{
+putchar (i);
+i++;
+}
+putchar (1);
+return (0);
+}
