184f18c4a05fa0038b8f74fa34a0bd320a834418
Nigussie1234 <nigussieadem@gmail.com>
2021-06-21 06:47:06 -0700

    Variables if else  while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print lowercase  letters of hexadecimal
+*
+*Description: prints lowercase letters of hexadecimal
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+int j;
+for (j = 97; j <= 102; j++)
+{
+putchar (j);
+}
+putchar (10);
+return (0);
+}
