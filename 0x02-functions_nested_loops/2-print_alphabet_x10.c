dac6810f72c9b71a49ee8df7374c247fdec5bb66
Nigussie1234 <nigussieadem@gmail.com>
2021-06-22 11:23:11 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
-*main - prints 10 times lowercase letters
+*prin_alphabet_x10 - prints 10 times lowercase letters
 *
 *Description: prints 10 times lowercase letters
 *
-*Return: 0 (success)
+*Return: void
 */
 void print_alphabet_x10(void)
 {
 int x;
 int y;
-for (x =0; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
 for (y = 97; y <= 122; y++)
 {
