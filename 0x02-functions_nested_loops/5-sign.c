f62371232fe305e3b0813f63cc9c9f947b94f8ce
Nigussie1234 <nigussieadem@gmail.com>
2021-06-22 12:08:28 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-Return (1);
+return (1);
 _putchar ('+');
 }
 else if (n == 0)
 {
-Return (0);
+return (0);
 _putchar ('0');
 }
 else
 {
-Return (-1);
+return (-1);
 _putchar ('-');
 }
 }
