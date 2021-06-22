56ca5ea8ca8b41d4465f914b1c7921cd9a98608a
Nigussie1234 <nigussieadem@gmail.com>
2021-06-22 07:52:29 -0700

    Functions nested loops:

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_islower checks if c is a lowercase
+*
+*Return: 1 if success
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return  (0);
+}
+}
