35b49cab1b566705d5b3253f46848b03c852074d
Nigussie1234 <nigussieadem@gmail.com>
2021-06-23 00:43:20 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*print_to_98 - prints numbers from n to 98
+*
+*Description: prints numbers from n to 98
+*
+*Return: void
+*/
+void print_to_98(int n)
+{
+if (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+else if (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("98\n");
+}
