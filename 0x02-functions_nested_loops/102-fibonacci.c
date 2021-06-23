6bd7189b357e208ddcec35f68d53c7d62735acc3
Nigussie1234 <nigussieadem@gmail.com>
2021-06-23 05:43:08 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - prints  the first 50 fibonacci  numbers
+*
+*Description: prints the first 50 fibonacci  numbers
+*
+*Return: 0
+*/
+int main(void)
+{
+int n, first = 1, second = 2, next;
+for (n = 1; n <= 50; n++)
+{
+next = first + second;
+first = second;
+second = next;
+}
+printf("%d\n", next);
+return (0);
+}
