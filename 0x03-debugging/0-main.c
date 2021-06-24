bf03968b3822408e640dff650712b2f747b2f79a
Nigussie1234 <nigussieadem@gmail.com>
2021-06-24 12:32:20 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,18 @@
+carrie@ubuntu:/debugging$ cat main.c
 #include "holberton.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+*positive_or_negative - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
 void positive_or_negative(int i)
 {
-int i;
+        int i;
 
-i = 0;
-positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-return (0);
+        return (0);
 }
+carrie@ubuntu:/debugging$
