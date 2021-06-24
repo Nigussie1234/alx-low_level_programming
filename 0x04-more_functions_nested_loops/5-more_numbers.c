d1bb60acc6153640e0d79743f7836b7bf29e2960
Nigussie1234 <nigussieadem@gmail.com>
2021-06-24 09:53:59 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *more_numbers - prints 10 times the numbers (0 to 14)
+ *
+ *
+ *Return: void
+ */
+void more_numbers(void)
+{
+int x;
+int y:
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+_putchar (y);
+}
+_putchar (x); 
+}
+_putchar ('\n');
+}
