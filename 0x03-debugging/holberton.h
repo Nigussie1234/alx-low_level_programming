637102dbd0c17a8ba84399d20d2fe3f18a3e2555
Nigussie1234 <nigussieadem@gmail.com>
2021-06-24 05:42:56 -0700

    debugging

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,3 +1,4 @@
+carrie@ubuntu:/debugging$ cat holberton.h
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
@@ -6,3 +7,4 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
+carrie@ubuntu:/debugging$
