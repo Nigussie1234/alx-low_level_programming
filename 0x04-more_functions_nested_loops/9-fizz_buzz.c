1e469e3bd79189eeb4585afebd9f62a9a496f4f6
Nigussie1234 <nigussieadem@gmail.com>
2021-06-25 13:15:51 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - take the fizz-fuzz challenge
+ *
+ *
+ *Return: 0
+ */
+void main(void)
+{
+int n;
+for (n = 1; n <= 100; n++)
+{
+if (n % 5 == 0 && n % 3 == 0)
+printf("FizzBuzz ");
+else if (n % 5 == 0)
+if (n == 100)
+printf("Buzz");
+else
+printf("Buzz ");
+else if (n % 3 == 0)
+printf("Fizz ");
+else
+printf("%d ", n);
+}
+printf("\n");
+return (0);
+}
