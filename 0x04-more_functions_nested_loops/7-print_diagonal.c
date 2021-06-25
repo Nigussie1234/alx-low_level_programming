25a3bac3d4a16a784e664aebacc129fe11a9a049
Nigussie1234 <nigussieadem@gmail.com>
2021-06-25 01:14:36 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,8 @@ void print_diagonal(int n)
 {
 int l;
 int m;
+if (n > 0)
+{
 for (l = 0; l < n; l++)
 {
 for (m = 0; m < l; m++)
@@ -18,5 +20,8 @@ _putchar (' ')
 _putchar ('\\');
 _putchar ('\n');
 }
+else
+{
 _putchar ('\n');
+}
 }
