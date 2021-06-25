07f762fb3432c0777b4addbebaf8b8fd517a0377
Nigussie1234 <nigussieadem@gmail.com>
2021-06-24 23:33:36 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_line - draws a straight line
+ *
+ *
+ *Return: void
+ */
+void print_line(int n)
+{
+int l;
+for (l = 0; i < n ; l++)
+{
+_putchar ('_');
+}
+_putchar ('\n');
+}
