755023ddaf0637ba090e71f6bdac6590a6fadabd
Nigussie1234 <nigussieadem@gmail.com>
2021-06-25 01:54:17 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_square - print a square followed by a new line
+ *@size: the size of a square
+ *
+ *Return: void
+ */
+void print_diagonal(int size)
+{
+int l = 0;
+if (size > 0)
+{
+while (l < size)
+{
+_putchar ('#');
+l++;
+}
+}
+else
+{
+_putchar ('\n');
+}
+}
