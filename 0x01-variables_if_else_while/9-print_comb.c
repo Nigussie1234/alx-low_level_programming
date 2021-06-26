51674532339632c849f76f0cd48f26e7e574cf92
Nigussie1234 <nigussieadem@gmail.com>
2021-06-26 04:34:54 -0700

    Variables if else while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,21 @@
 */
 int main(void)
 {
-	int i;
+int i;
 
-	i = 48;
+i = 48;
 
-	while (i < 58)
-	{
-		putchar(i);
-		if (i != 57)
-		{
-		  putchar(44);
-			putchar(32);
-       	}
-		i++;
-	}
+while (i < 58)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+i++;
+}
 
-	putchar(10);
-	return (0);
+putchar(10);
+return (0);
 }
