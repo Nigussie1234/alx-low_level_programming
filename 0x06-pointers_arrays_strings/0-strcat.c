1d426638a2418e6dd507b76fff300c0547784f74
Nigussie1234 <nigussieadem@gmail.com>
2021-06-30 07:43:08 -0700

    Pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return - pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+int n;
+strncat(dest, src, n);
+return (dest);
+}
