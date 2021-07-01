c7c08d394a697a4517abdebcd990479132a8dac8
Nigussie1234 <nigussieadem@gmail.com>
2021-07-01 07:09:57 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,20 @@
 int _strcmp(char *s1, char *s2)
 {
 int n = 0;
-while (*s1 != '\0' && *s2 != '\0')
+int m = 0;
+while (s1[n] != '\0' && s2[n] != '\0')
 {
-if (*s1 != *s2)
+if (s1[n] != s2[n])
 {
 m = 1;
+break;
 }
-s1++;
-s2++;
+n++;
 }
 if (m == 0)
 {
-return 0;
+return (0);
 }
 else
-return 1;
+return (1);
 }
