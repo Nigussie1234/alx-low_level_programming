d987b62b22207515e5a59e790af54335ec06889d
Nigussie1234 <nigussieadem@gmail.com>
2021-06-30 21:47:47 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,21 @@
-#include "holberton.h"
+x#include "holberton.h"
 
 /**
- * upper_string - changes all lowercase letters of a string to uppercase
+ * upper_string - changes lowercase  string to uppercase
  * @s: is a string
  * Return: void
  */
 
 char *string_toupper(char *s)
 {
-int c = 0;
+int c;
    
-while (s[c] != '\0')
+for (c = 0; s[c] != '\0'; c++)
 {
 if (s[c] >= 'a' && s[c] <= 'z')
 {
-
 s[c] = s[c] - 32;
 }
-c++;
 }
 return (s);
 }
