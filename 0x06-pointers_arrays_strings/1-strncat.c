c9cc99a51a0337406bf6fff5af407813bdd036f7
Nigussie1234 <nigussieadem@gmail.com>
2021-06-30 18:39:34 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,15 @@
 
 char *_strncat(char *dest, char *src, int n);
 {
-char i = 0;
-char j;
+int i = 0;
+int j;
 while (dest[i] != '\0')
 {
 i++;
 }
 for (j = 0; j < n && src[j] != '\0'; j++)
 {
-dest[i + j] = src[i];
+dest[i + j] = src[j];
 }
 dest[i + j] = '\0';
 return (dest);
