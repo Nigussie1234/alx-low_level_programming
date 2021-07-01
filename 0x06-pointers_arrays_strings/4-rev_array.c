b83261834134648b2c3968a0ca691e5ebbeeced0
Nigussie1234 <nigussieadem@gmail.com>
2021-06-30 20:07:56 -0700

    Pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array
+ * @n: is an integer
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+int c, t;
+
+for (c = 0; c < n/2; c++)
+{
+t = a[c];
+a[c] = a[n-c-1];
+a[n-c-1] = t;
+}
+}
