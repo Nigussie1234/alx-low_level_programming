f6b615b9a9df874417ef84dea1410c54092d9cbc
Nigussie1234 <nigussieadem@gmail.com>
2021-07-04 11:23:19 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
   
 {
 int days_in_feb = 28;
-if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 {
 days_in_feb = 29;
 }
@@ -55,19 +55,5 @@ case 12:
 day += 31+days_in_feb+31+30+31+30+31+31+30+31+30;            
 break;                                    
 }
-return (day); 
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
+return (day);
 }
