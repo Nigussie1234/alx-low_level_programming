898faefb6de0297c0b9f645d50837661e8f4e9c9
Nigussie1234 <nigussieadem@gmail.com>
2021-07-07 06:19:57 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -27,18 +27,3 @@ largest = c;
 
 return (largest);
 }
-int main(void)
-{
-int a, b, c;
-int largest;
-
-a = 972;
-b = -98;
-c = 0;
-
-largest = largest_number(a, b, c);
-
-printf("%d is the largest number\n", largest);
-
-return (0);
-}
