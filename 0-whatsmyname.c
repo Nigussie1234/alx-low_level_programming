57b34aab1666701e538642077c370986e3b81baf
Nigussie1234 <nigussieadem@gmail.com>
2021-07-12 04:32:27 -0700

    prints its name, followed by a new line

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints its name, followed by a new line
+ *
+ *Description: prints its name, followed by a new line
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+_putchar (argv[0]);
+_putchar ('\n');
+ return (0);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main()
+{
+int i, n;
+i = n / 2; 
+/**
+ *is_prime_number - returns 1 if the input integer is a prime number
+ *@n: integer
+ *
+ *Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+  if (i == 1)
+    {
+      return (1);
+    }
+  
