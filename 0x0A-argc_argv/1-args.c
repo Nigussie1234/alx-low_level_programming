87d3affdc432a8a2ca8024bf686a4ed1bb7f5ffb
Nigussie1234 <nigussieadem@gmail.com>
2021-07-12 06:49:30 -0700

    the number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - prints the number of command line arguments, followed by a new line
+ *@argc: argument count
+ *@argv: argument vector
+ *Description: prints its name, followed by a new line
+ *
+ *Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argc);
+
+return (0);
+}
