9048b87f6780ec0d895506e0efca4f32387470bc
Nigussie1234 <nigussieadem@gmail.com>
2021-07-15 07:44:19 -0700

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - allocates memory using malloc
+ *
+ *@b: integer
+ *
+ *Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *king
+king = malloc(b);
+if (king == NULL)
+{
+exit (98);
+}
+return (king);
+}
