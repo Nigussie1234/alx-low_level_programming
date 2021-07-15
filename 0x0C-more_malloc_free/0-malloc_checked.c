179259b1633f86a03f7b4e5b3094f63ae416174e
Nigussie1234 <nigussieadem@gmail.com>
2021-07-15 10:20:58 -0700

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
+void *king;
 
-ptr = malloc(b);
-if (ptr == NULL)
+king = malloc(b);
+if (king == NULL)
 exit(98);
-return (ptr);
+return (king);
 }
