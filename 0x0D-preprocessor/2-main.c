c389df70c54093b4d0b1d721e7f3e9270ac2f596
Nigussie1234 <nigussieadem@gmail.com>
2021-07-20 07:45:35 -0700

    a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints the name of the file it was compiled from
+ *
+ *Description: prints the name of the file it was compiled from
+ *
+ *Return: 0
+ */
+
+void main()
+{
+printf("%s\n", __FILE__);
+return (0);
+}
