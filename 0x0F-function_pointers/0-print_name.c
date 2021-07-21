ad0fc8f052563fe9862eabe7fb236d588427b023
Nigussie1234 <nigussieadem@gmail.com>
2021-07-21 07:18:29 -0700

    a function prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -6,7 +7,9 @@
  * @f: print name as is and UPPERCASE.
  * Return: Always 0.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }
