b0399185b292263eddef00d877b21787e04fe983
Nigussie1234 <nigussieadem@gmail.com>
2021-07-22 11:20:39 -0700

    64-bit program in assembly that prints Hello, Holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,12 @@
-	section .data
-	msg db "Hello,Holberton",10 ; 10 is the ASCII code for a new line (LF)
-
 	section .text
-	global _start
-
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 13
-	syscall
-
-	mov rax, 60
-	mov rdi, 0
-	    syscall
+	global main
+main:
+	mov ebx,1
+	mov ecx,msg
+	mov edx,17
+	mov eax,4
+	int 0x80
+	mov eax,0
+	int 0x80
+	section .data
+	msg db 'Hello, Holberton',0xa
