8d410e4b3a0add9b4c0a181dca000f703ea8ed1f
Nigussie1234 <nigussieadem@gmail.com>
2021-07-23 04:46:07 -0700

    This file should contain the function that selects the correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,13 +18,14 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
-for (i = 0; ops[i].op != NULL; i++)
+int i = 0;
+while (ops[i].op != NULL)
 {
 if (*ops[i].op == *s && s[1] == '\0')
 {
 return (ops[i].f);
 }
+i++;
 }
 return (NULL);
 }
