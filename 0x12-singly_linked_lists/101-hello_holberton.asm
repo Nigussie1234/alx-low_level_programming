afb36e2e8bce61df7f9e7996862536b14499bf3a
Nigussie1234 <nigussieadem@gmail.com>
2021-07-30 10:51:14 -0700

    64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,19 @@
-	section .text 
-	global _start 		; must be declared for using gcc
-	_start 			; tell linker entry point
-	mov edx, len 		; message length
-	mov edx, msg 		; message to write
-	mov ebx, 1		; file descriptor
-	mov eax, 4 		; system call number
-	int 0x80 		; call kernel
 	section .data
-	msg db 'Hello, Holberton', 0xa ; our string
-	len equ $ - msg		       ; length of our string
-	
+hello:		db "Hello, Holberton",10,0
+format:		db "%s",0
+
+	section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	mov rsi, hello
+	mov rdi, format
+	call printf
+
+	pop rbp
+	mov rax, 0
+	    ret
